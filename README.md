97b583e969a94befa93a64cb16a12f9bb94decac
wbhouston <wmbhouston@gmail.com>
2022-09-11 10:48:05 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-xsktyg
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-xsktyg)
---
+++ src/lib/Counter.svelte
@@ -0,0 +1,10 @@
+<script>
+  let count = 0
+  const increment = () => {
+    count += 1
+  }
+</script>
+
+<button style='border-1 px-2 py-1 font-bold' on:click={increment}>
+  count is {count}
+</button>
